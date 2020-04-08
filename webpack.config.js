0e201b11f54c799614a3db896fde0fccd59ea6d9
nezouse <stankiewicz1998@gmail.com>
2020-04-08 21:18:19 +0200

    Add sass-loader

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,10 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.sass$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
   plugins: [
